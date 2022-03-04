bc1ba3a2a98dc2214266e34f994527deca03a318
Alexa Ped <alexaped16@gmail.com>
2022-03-04 10:50:14 -0600

    first commit

---
+++ Shoppingcart.py
@@ -0,0 +1,16 @@
+#shopping cart for goth angel 
+
+shopping_active = True
+
+while shopping_active:
+
+ def primary_page():
+    print("Welcome to Goth Angel! Your cart is currently empty")
+    user_cart = {}
+
+    
+       #get user information
+    name = input("What's your name?")
+    for_who = input("Are you shopping for yourself or for someone else")
+
+
