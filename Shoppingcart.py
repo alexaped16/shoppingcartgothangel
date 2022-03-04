7b9adc185ec4655be9334ab288bdb5cf4487d2b5
Alexa Ped <alexaped16@gmail.com>
2022-03-04 15:33:42 -0600

    second commit

--- Shoppingcart.py
+++ Shoppingcart.py
@@ -1,16 +1,25 @@
 #shopping cart for goth angel 
 
-shopping_active = True
 
-while shopping_active:
 
- def primary_page():
-    print("Welcome to Goth Angel! Your cart is currently empty")
-    user_cart = {}
 
-    
-       #get user information
-    name = input("What's your name?")
-    for_who = input("Are you shopping for yourself or for someone else")
+
+def primary_page():
+   print("Welcome to Goth Angel! Your cart is currently empty. ")
+   user_cart = {}
+   options = {"add", "remove", "clear", "quit"}
+   shopping_active = True
+   while shopping_active:
+      answer = input("What would you like to do? Add/Remove/Clear/Quit ")
+      while answer not in options: 
+         answer = input("Sorry, that is not an acceptable answer. Would you like to: Add/Remove/Clear/Quit ")
+         if answer == "quit":
+            break
+         if answer == 'add':
+            break
+
+def add_items():
+   item_type: input("What kind of jewelry are you looking for - Necklaces, rings, or earings?")
+   
 
 
---
+++ practice.py
@@ -0,0 +1,76 @@
+
+#global variables
+shopping_cart = {}
+options = {'1','2','3','4'}
+item_type = {'necklace', 'ring', 'earings', 'a necklace', 'a ring' }
+
+
+def primary_page():
+
+    #welcome message 
+    print("Welcome to Goth Angel! Your cart is currently empty. ")
+
+
+    user_active = True
+    while user_active == True:
+
+        print("""
+        Would you like to: 
+        ----------------
+        1: Add item
+        2. Remove item
+        3: View cart
+        4: Exit 
+
+        """)
+
+        option = input("Please enter the number of the option you'd like to choose: ")
+        if option == '4':
+            print("Thanks for shopping. Come back soon!")
+            user_active = False
+            break
+        elif option == '1': 
+            add_item(shopping_cart)
+        elif option == '2':
+            remove_item(shopping_cart)
+        elif option == '3':
+            view_cart(shopping_cart)
+        else: 
+            print("Invalid request. Please enter a valid number: ")
+    
+    
+def add_item(cart):
+    item_type = input("Would you like to buy a necklace, a ring, or earings. Enter an item: ")
+    if item_type in cart:
+        repeat_item = input("Item already in shopping cart. Would you like to add it again? Yes or No ")
+        if repeat_item == 'yes':
+            cart[item_type] = cart[item_type] +1
+        elif repeat_item == 'no':
+            print("Okay, this item will not be added to your cart")   
+    else: 
+        add_to_cart = input("Good choice. This item has been added to your cart. ")
+        cart[item_type] = cart[item_type] +1
+        
+        
+
+  
+
+def remove_item(cart):
+    item_type = input("Enter the name of the item you'd like to remove: ")
+    print("Are you sure you would like to remove this item? Yes or No")
+    if item_type in {'necklace','earings','ring'}:
+        del(cart[item_type])
+
+
+def view_cart(cart): 
+    for item in shopping_cart:
+        print("---Current Shopping Cart----")
+        print(item_type, ":",cart[item_type])
+
+primary_page()
+    
+            
+
+
+    
+
